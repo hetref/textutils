c55f63bd8b1150f290c0cffba4e6235660aa1560
hetref <70653460+hetref@users.noreply.github.com>
2021-12-08 14:19:12 +0530

    deploy my react textutils website to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=> {
+    setAlert({
+      msg: message,
+      type: type
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 2500);
+  }
+
+  const toggleMode = ()=> {
+    if(mode === 'light') {
+      setMode("dark");
+      document.body.style.backgroundColor = 'black';
+      document.body.style.color = 'white';
+      showAlert("Dark Mode has been enabled", "success");
+    } else {
+      setMode("light")
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+      showAlert("Light Mode has been enabled", "success");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert} />
+    <div className="container my-3">
+      <TextForm heading="Enter the text below to Analyse" showAlert={showAlert} />
+      {/* <About /> */}
     </div>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const [text, setText] = useState("");
+
+  var timeToRead = 0.008 * text.split(" ").length;
+
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to Uppercase!", "success");
+  };
+
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to Lowercase!", "success");
+  };
+
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Cleared Up the text!", "success");
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Extra spaces removed from your text!", "success");
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h2>{props.heading}</h2>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="textBox"
+            rows="8"
+            value={text}
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <button className={"mx-1 my-1 btn btn-success"} onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button
+          className={"mx-1 my-1 btn btn-success"}
+          onClick={handleLowClick}
+        >
+          Convert to Lowercase
+        </button>
+        <button
+          className={"mx-1 my-1 btn btn-success"}
+          onClick={handleExtraSpaces}
+        >
+          Remove Extra
+        </button>
+        <button
+          className={"mx-1 my-1 btn btn-success"}
+          onClick={handleClearClick}
+        >
+          Clear Text
+        </button>
+      </div>
+      <div className="container my-3">
+        <h2>Your Text Summary</h2>
+        {/* <p> {text.split(" ").length} Words and {text.length} Characters</p> */}
+        <p>
+          {
+            text
+              .trim()
+              .split(" ")
+              .filter(function (element) {
+                return element !== "";
+              }).length
+          }{" "}
+          Words and {text.length} Characters.
+        </p>
+        <p>
+          Average time the text will take to read is - {timeToRead.toFixed(3)} Minutes
+        </p>
+        <h2>Preview</h2>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter something in the textbox above to preview it here!"}
+        </p>
+      </div>
+    </>
+  );
+}
